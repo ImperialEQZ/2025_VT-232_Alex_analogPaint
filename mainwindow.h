e5846d0eb5838167d7f3f91b1e7346e8a9f46387
ImperialEQZ <157801022+ImperialEQZ@users.noreply.github.com>
2025-04-16 21:54:19 +0300

    Добавил возможные действия и события

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QList>
+
+class DrawingArea;//"холст" программы
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,5 +22,39 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+protected:
+    void endEvent(QCloseEvent *event) override;//функция, завершающая события
+
+
+private slots://Возможные события, список "слотов"
+    void open();
+    void save();
+    void changePenColor();
+    void changePenWidth();
+    void aboutPaintAnalog();
+
+private://Действия пользователя
+    void createActions();//создать действие
+    void createMenu();//меню с разными событиями
+    bool saveFile(const QByteArray &formatFile);//сохранить в выбранном формате
+    bool abilitySave();//Возможность сохранить файл
+
+    DrawingArea *DrawingArea;
+//Список виджетов-меню
+    QMenu *saveAs;//сохранить файл как
+    QMenu *menuFile;//
+    QMenu *menuOption;//меню параметров (пример, смена цвета)
+    QMenu *menuHelp;//меню о программе PaintAnalog
+
+    QAction *openActions;//Доступные действия
+
+    QList<QAction *> saveAsActions;//список действий
+    QAction *penColorActions;//изменить цвет пера
+    QAction *printActions;//печать
+    QAction *penWidthActions;//изменить ширину пера
+    QAction *clearAreaActions;//очистить холст
+    QAction *aboutActions;//открытие диалогового окна
+
 };
 #endif // MAINWINDOW_H
