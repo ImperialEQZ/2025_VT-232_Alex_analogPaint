bb550e60182fe2b1f72598958c702d0209690166
ImperialEQZ <157801022+ImperialEQZ@users.noreply.github.com>
2025-04-16 21:51:31 +0300

    Рефлизация действий и событий

---
+++ build/gcc-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_PaintAnalog_FILE D:/QT Project/PaintAnalog/build/gcc-Debug/PaintAnalog.exe)
+set(__QT_DEPLOY_TARGET_PaintAnalog_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_PaintAnalog_RUNTIME_DLLS D:/msys64/mingw64/bin/Qt6Widgets.dll;D:/msys64/mingw64/bin/Qt6Gui.dll;D:/msys64/mingw64/bin/Qt6Core.dll)
