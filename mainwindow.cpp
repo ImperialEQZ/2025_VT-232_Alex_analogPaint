2124343de732fe4f1560afde2255408d2f411eb9
ImperialEQZ <157801022+ImperialEQZ@users.noreply.github.com>
2025-04-17 10:59:12 +0300

    Реализация действий и создания меню (доработать)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -54,3 +54,57 @@ void MainWindow::about() {
     QMessageBox::about(this, tr("О приложении"), tr("Вышло прикольно"));
 }
 
+void MainWindow::createActions() {
+    openActions = new QAction(tr("&Open"), this);
+    openActions->setShortcut(QKeySequence::Open);
+    connect(openActions, SIGNAL(triggered()), this, SLOT(open()));
+    foreach (QByteArray format, QImageWriter::supportedImageFormats()){
+        QString text = tr("%1...").arg(QString(format).toUpper());
+        QAction *action = new QAction(text, this);
+        action->setData(format);
+        connect(action, SIGNAL(triggered()), this, SLOT(save()));
+        saveAsActions.append(action);
+    }
+    printActions = newQAction(tr("&Print..."), this);
+    connect(printActions, SIGNAL(triggered()), drawningArea, SLOT(print()));
+    exitActions = new QAction(tr("&Exit"), this);
+    exitActions->setShortcuts(QKeySequence::Quit);
+    connect(exitActions, SIGNAL(triggered()), this, SLOT(close()));
+
+    penColorActions = new QAction(tr("&Pen Color..."), this);
+    connect(penColorActions, SIGNAL(triggered()), drawningArea, SLOT(changePenColor()));
+
+    penWidthActions = new QAction(tr("&Pen Width..."), this);
+    connect(penWidthActions, SIGNAL(triggered()), drawningArea, SLOT(changePenWidth()));
+
+    clearAreaActions = new QAction(tr("&Clear Screen..."), this);
+    clearAreaActions->setShortcut(tr("Cntr+L"));
+    connect(penColorActions, SIGNAL(triggered()), drawningArea, SLOT(clearImage()));
+
+    aboutActions = new QAction(tr("&About..."), this);
+    connect(aboutActions, SIGNAL(triggered()), SLOT(about()));
+}
+
+void MainWindow::createMenu(){
+    saveAs = new QMenu(tr("&Save As"), this);
+    foreach(QAction *action, saveAsActions)
+        saveAs->addAction(action);
+    menuFile = new QMenu(tr("&File"), this);
+    menuFile->addAction(open);
+    menuFile->addAction(saveAs);
+    menuFile->addAction(printActions);
+    menuFile->addSeparator();
+    menuFile->addAction(exitActions);
+
+    menuOption = new QMenu(tr("&Options"), this);
+    menuOption->addAction(penColorActions);
+    menuOption->addAction(penWidthActions);
+
+    menuHelp = new QMenu(tr("&Help"), this);
+    menuHelp->addAction(aboutActions);
+
+    menuBar()->addMenu(menuFile);
+    menuBar()->addMenu(menuOption);
+    menuBar()->addMenu(menuHelp);
+}
+
