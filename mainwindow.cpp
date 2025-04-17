c43eefd2e291efb68c8fc6c350ca5e5a1c12e286
ImperialEQZ <157801022+ImperialEQZ@users.noreply.github.com>
2025-04-17 09:51:48 +0300

    Реализация слотов-событий в главном окне

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,56 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+//#include "./ui_mainwindow.h"
+#include "DrawingArea.h"
+#include <QtWidgets>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+
+MainWindow::MainWindow()
 {
-    ui->setupUi(this);
+    drawningArea = new PaintArea;
+    setCentralWidget(drawningArea);
+    createActions();
+    createMenu();
+    setWindowTitle(tr("Draw"));
+    resize(600, 600);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+void MainWindow::closeEvent(QCloseEvent *event) {
+    if(abilitySave()) {
+        event->accept();
+    } else {
+        event->ignore();
+    }
 }
+
+void MainWindow::open() {
+    if(abilitySave()) {
+        QString fileName = QFileDialog::getOpenFileName(this, tr("Open file"), QDir::currentPath());
+        if (!fileName.isEmpty()) {
+            drawningArea->openImage(fileName);
+        }
+    }
+}
+
+void MainWindow::abilitySave() {
+    QAction *act = qobject_cast<QAction *> (sender());
+    QByte fileFormat = act->data().toByteArray();
+    saveFile(fileFormat);
+}
+
+void MainWindow::penColor() {
+    QColor newColor = QColorDialog::getColor(drawningArea->penColor());
+    if(newColor.isValid()) {
+        drawningArea->setPenColor(newColor);
+    }
+}
+
+void MainWindow::penWidth() {
+    bool flag;
+    int newWidth = QInputDialog::getInt(this, tr("Draw"), tr("Выберите толщину пера:"),
+                                        drawningArea->penWidth(), 1, 60, 1, &flag);
+}
+
+void MainWindow::about() {
+    QMessageBox::about(this, tr("О приложении"), tr("Вышло прикольно"));
+}
+
